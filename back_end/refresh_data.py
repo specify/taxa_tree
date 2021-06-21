03b6790ab0868f79b92f42b2cb13d59d4aee67a6
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-06-21 22:40:49 +0300

    Extract back-end config into a separate file

--- back_end/refresh_data.py
+++ back_end/refresh_data.py
@@ -2,52 +2,54 @@ import requests
 import os
 from zipfile import ZipFile
 from pathlib import Path
+from config import site_link, target_dir, \
+        mysql_host, mysql_user, mysql_password
 
-# Config
-site_link = 'http://localhost:80/'
-target_dir = '/Users/mambo/Downloads/python-taxonomy/'
+
+print('Config')
 source_url = 'https://www.itis.gov/downloads/itisMySQLBulk.zip'
-mysql_host = 'localhost'
-mysql_user = 'root'
-mysql_password = 'root'
 
-# # Downloading the archive
-# Path(target_dir).mkdir(parents=True, exist_ok=True)
-#
-# archive_name = target_dir + 'archive.zip'
-#
-# request = requests.get(source_url)
-#
-# if os.path.exists(archive_name):
-#
-#     with open(archive_name, 'rb') as file:
-#         old_archive_content = file.read()
-#
-#     if request.content == old_archive_content:
-#         raise SystemExit('No need to refresh data')
-#
-# with open(archive_name, 'wb') as file:
-#     file.write(request.content)
-#
-#
-# # Unzipping the file
-# with ZipFile(archive_name, 'r') as zip_file:
-#
-#     files = zip_file.namelist()
-#     directory_name = files[0]
-#     directory_name = directory_name[0:directory_name.find('/')]
-#
-#     zip_file.extractall(target_dir)
-#
-# # Putting new data into the database
-# os.system('cd %s && mysql -h%s -u%s -p%s < %s' % (
-#     directory_name,
-#     mysql_host,
-#     mysql_user,
-#     mysql_password,
-#     'CreateDB.sql'))
 
-# Extract the data
+print('Downloading the archive')
+Path(target_dir).mkdir(parents=True, exist_ok=True)
+
+archive_name = target_dir + 'archive.zip'
+
+request = requests.get(source_url)
+
+if os.path.exists(archive_name):
+
+    with open(archive_name, 'rb') as file:
+        old_archive_content = file.read()
+
+    if request.content == old_archive_content:
+        raise SystemExit('No need to refresh data')
+
+with open(archive_name, 'wb') as file:
+    file.write(request.content)
+
+
+print('Unzipping the file')
+with ZipFile(archive_name, 'r') as zip_file:
+
+    files = zip_file.namelist()
+    directory_name = files[0]
+    directory_name = directory_name[0:directory_name.find('/')]
+
+    zip_file.extractall(target_dir)
+
+
+print('Putting new data into the database')
+os.system('cd %s && mysql -h%s -u%s -p%s < %s' % (
+    directory_name,
+    mysql_host,
+    mysql_user,
+    mysql_password,
+    'CreateDB.sql')
+)
+
+
+print('Extract the data')
 
 queries = ['kingdoms', 'ranks', 'rows']
 
@@ -66,5 +68,5 @@ for query in queries:
         target_dir + query + '.csv'
     ))
 
-# Let PHP handle the rest -_-
+print('Let PHP handle the rest -_-')
 requests.get(url=site_link+'cron/refresh_data.php')
