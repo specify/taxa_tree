cab3945de0e4f715eafa4a68c1b6d32e521453cf
Max Patiiuk <max@patii.uk>
2021-09-24 14:27:08 -0500

    Update back_end to handle new data format

--- back_end/config.py
+++ back_end/config.py
@@ -1,6 +1,21 @@
 site_link = 'http://localhost:80/'
-target_dir = '/Users/mambo/Downloads/gbif_col/'
+target_dir = '/Users/maxxxxxdlp/Downloads/col_taxon'
 mysql_host = 'localhost'
 mysql_user = 'root'
 mysql_password = 'root'
 
+
+mysql_mode = 'docker'
+
+if mysql_mode == 'docker':
+    docker_container = 'test'
+    mysql_command = f'docker exec {docker_container} mysql'
+    mysql_export_command = f'docker exec {docker_container} mysql '
+    mysql_command_copy_suffix = '\';'
+    docker_dir = '/sql/col_taxon/'
+else:
+    docker_container = ''
+    mysql_command = 'mysql'
+    mysql_command_copy = 'docker exec test /bin/bash -c \'echo mysql '
+    mysql_command_copy_suffix = '\';'
+    docker_dir = target_dir
--- back_end/refresh_data.py
+++ back_end/refresh_data.py
@@ -2,28 +2,30 @@ import requests
 import xml.etree.ElementTree as xmlParser
 import json
 import time
+import os
 from zipfile import ZipFile
 from pathlib import Path
-from os import system, path
-from config import site_link, target_dir, \
-        mysql_host, mysql_user, mysql_password
+from config import target_dir, mysql_host, mysql_user, mysql_password, \
+    mysql_command, docker_dir, mysql_export_command
 
 #
-source_url = 'http://www.catalogueoflife.org/DCA_Export/zip/archive-complete.zip'
+
+get_source_url = lambda date:\
+    f'https://download.catalogueoflife.org/col/monthly/{date}_coldp.zip'
 meta_url = 'https://api.gbif.org/v1/dataset/7ddf754f-d193-4cc9-b351-99906754a03b/document'
 
 #
 begin_time = time.time()
 print('Preparation')
 Path(target_dir).mkdir(parents=True, exist_ok=True)
-
+"""
 
 #
 print('Downloading meta data')
 
-date_destination = target_dir + 'date.txt'
-meta_destination = target_dir + 'meta.xml'
-temp_meta_destination = target_dir + 'meta_temp.xml'
+date_destination = os.path.join(target_dir, 'date.txt')
+meta_destination = os.path.join(target_dir, 'meta.xml')
+temp_meta_destination = os.path.join(target_dir, 'meta_temp.xml')
 
 request = requests.get(meta_url)
 
@@ -35,14 +37,12 @@ def get_date(target_destination):
     return description.find('pubDate').text.strip()
 
 
-if path.exists(meta_destination):
+with open(temp_meta_destination, 'wb') as file:
+    file.write(request.content)
+new_data_date = get_date(temp_meta_destination)
 
-    with open(temp_meta_destination, 'wb') as file:
-        file.write(request.content)
-
+if os.path.exists(meta_destination):
     old_data_date = get_date(meta_destination)
-    new_data_date = get_date(temp_meta_destination)
-
     if old_data_date == new_data_date:
         raise SystemExit('No need to refresh data')
 
@@ -54,9 +54,9 @@ with open(date_destination, 'w') as file:
 
 print('Downloading the archive')
 
-archive_name = target_dir + 'archive.zip'
+archive_name = os.path.join(target_dir, 'archive.zip')
 
-request = requests.get(source_url)
+request = requests.get(get_source_url(new_data_date))
 
 with open(archive_name, 'wb') as file:
     file.write(request.content)
@@ -66,57 +66,87 @@ with open(archive_name, 'wb') as file:
 print('Unzipping the file')
 with ZipFile(archive_name, 'r') as zip_file:
     files = zip_file.namelist()
-    zip_file.extractall(target_dir+'extracted/')
-
-
+    zip_file.extractall(os.path.join(target_dir, 'extracted/'))
 
 print('Creating database schema')
-with open('sql/taxa.sql', 'r') as file:
+with open('sql/schema.sql', 'r') as file:
     result_query = file.read()
 
 result_query = result_query.replace("\n", ' ')
 result_query = result_query.replace("`", '\\' + '`')
 
-system('mysql -h%s -u%s -p%s -e "%s"' % (
+command = '%s -h%s -u%s -p%s -e "%s"' % (
+    mysql_command,
     mysql_host,
     mysql_user,
     mysql_password,
     result_query
-))
-
+)
+print(command)
+os.system(command)
 
 #
 print('Putting new data into the database')
-system('mysql -h%s -u%s -p%s --database gbif_col -e "LOAD DATA LOCAL INFILE \'%s\' INTO TABLE taxa IGNORE 1 LINES;"' % (
-    mysql_host,
-    mysql_user,
-    mysql_password,
-    target_dir+'extracted/taxa.txt'))
+print('[NameUsage] (this would take some time)')
+os.system(
+    '%s -h%s -u%s -p%s --database gbif_col -e "LOAD DATA LOCAL INFILE \'%s\' INTO TABLE NameUsage IGNORE 1 LINES;"' % (
+        mysql_command,
+        mysql_host,
+        mysql_user,
+        mysql_password,
+        os.path.join(docker_dir, 'extracted/NameUsage.tsv')
+    )
+)
 
+print('[NameRelation]')
+os.system(
+    '%s -h%s -u%s -p%s --database gbif_col -e "LOAD DATA LOCAL INFILE \'%s\' INTO TABLE NameRelation IGNORE 1 LINES;"' % (
+        mysql_command,
+        mysql_host,
+        mysql_user,
+        mysql_password,
+        os.path.join(docker_dir, 'extracted/NameRelation.tsv')
+    )
+)
+
+print('[Reference] (this would take some time)')
+os.system(
+    '%s -h%s -u%s -p%s --database gbif_col -e "LOAD DATA LOCAL INFILE \'%s\' INTO TABLE Reference IGNORE 1 LINES;"' % (
+        mysql_command,
+        mysql_host,
+        mysql_user,
+        mysql_password,
+        os.path.join(docker_dir, 'extracted/Reference.tsv')
+    )
+)
 
 #
-print('Extracting data (this may take some time)')
+print('Extracting data (this would take some time)')
 with open('sql/rows.sql', 'r') as file:
     result_query = file.read()
 
 result_query = result_query.replace("\n", ' ')
-result_query = result_query.replace("`", '\\' + '`')
+result_query = result_query.replace("'", '"')
 
-system('mysql -h%s -u%s -p%s -e "%s" > %s' % (
-    mysql_host,
-    mysql_user,
-    mysql_password,
-    result_query,
-    target_dir + 'rows.csv'
-))
-
+"""
+rows_data = os.path.join(target_dir, 'rows.csv')
+"""
+out_file = os.path.join(docker_dir, 'rows.csv')
+if os.path.exists(rows_data):
+    # MariaDB would throw error instead of overwriting if file exists
+    os.remove(rows_data)
+os.system(
+    f'{mysql_export_command} '
+    f'-u{mysql_user} '
+    f'-p{mysql_password} '
+    f'-h{mysql_host} '
+    f'-e \'{result_query} INTO OUTFILE "{out_file}" FIELDS TERMINATED BY "\t"\''
+)
 
 #
+"""
 print('Building a list of kingdoms and ranks and a tree of rows (taxon units)')
-kingdoms_data = target_dir + 'kingdoms.json'
-ranks_data = target_dir + 'ranks.json'
-rows_data = target_dir + 'rows.csv'
-rows_destination = target_dir + 'rows/'
+rows_destination = os.path.join(target_dir, 'rows/')
 
 
 def list_flip(original_list):
@@ -130,162 +160,105 @@ def list_flip(original_list):
     return dictionary
 
 
-rows_file = open(rows_data, 'r')
-line = rows_file.readline()
 rows = {}
-ranks = {}
 kingdoms = {}
-root = {}
-columns = list_flip(['tsn', 'name', 'common_name', 'parent_tsn', 'rank', 'kingdom', 'author', 'source'])
+columns = list_flip([
+    'tsn', 'name', 'common_name', 'parent_tsn', 'rank', 'author', 'source'
+])
 line_number = 0
-specify_ranks = ['Domain', 'Infrakingdom', 'Superphylum', 'Infradivision', 'Cohort', 'Kingdom', 'Subkingdom', 'Division',
-               'Subdivision', 'Phylum', 'Subphylum', 'Superclass', 'Class', 'Subclass', 'Infraclass', 'Superorder',
-               'Order', 'Suborder', 'Infraorder', 'Superfamily', 'Family', 'Subfamily', 'Tribe', 'Subtribe', 'Genus',
-               'Subgenus', 'Section', 'Subsection', 'Species', 'Subspecies', 'Variety', 'Subvariety', 'Forma',
-               'Subforma']
+specify_ranks = [rank.lower() for rank in [
+    'Domain', 'Infrakingdom', 'Superphylum', 'Infradivision', 'Cohort',
+    'Kingdom', 'Subkingdom', 'Division', 'Subdivision', 'Phylum',
+    'Subphylum', 'Superclass', 'Class', 'Subclass', 'Infraclass', 'Superorder',
+    'Order', 'Suborder', 'Infraorder', 'Superfamily', 'Family', 'Subfamily',
+    'Tribe', 'Subtribe', 'Genus', 'Subgenus', 'Section', 'Subsection',
+    'Species', 'Subspecies', 'Variety', 'Subvariety', 'Forma', 'Subforma'
+]]
 
-while True:
+with open(rows_data, 'r') as rows_file:
     line = rows_file.readline()
+    while True:
+        line = rows_file.readline()
 
-    if not line:
-        break
+        if not line:
+            break
 
-    row = line.strip()
-    row = row.split("\t")
+        row = line.strip()
+        row = row.split("\t")
 
-    while len(row) < len(columns):
-        row.append('')
+        while len(row) < len(columns):
+            row.append('')
 
-    tsn = int(row[columns['tsn']])
-    parent_tsn = int(row[columns['parent_tsn']])
-    kingdom = row[columns['kingdom']]
-    rank = row[columns['rank']]
-    name = row[columns['name']]
+        tsn = row[columns['tsn']]
+        parent_tsn = row[columns['parent_tsn']]
+        rank = row[columns['rank']]
+        name = row[columns['name']]
 
-    # create kingdom
-    if kingdom not in kingdoms:
+        # create kingdom
+        if rank == 'kingdom':
+            kingdoms[tsn] = name
 
-        if not kingdoms:
-            kingdom_id = 1
-        else:
-            kingdom_id = kingdoms[max(kingdoms, key=kingdoms.get)] + 1
+        if rank not in specify_ranks:
+            continue
 
-        kingdoms[kingdom] = kingdom_id
+        rank_id = specify_ranks.index(rank) + 1
 
-        ranks[kingdom_id] = {}
-        rows[kingdom_id] = {}
+        if row[columns['common_name']] == name:
+            row[columns['common_name']] = ''
 
-    else:
-        kingdom_id = kingdoms[kingdom]
+        rows[tsn] = [
+            [
+                name,
+                row[columns['common_name']],
+                row[columns['author']],
+                row[columns['source']],
+            ],
+            rank_id,
+            [],  # children
+            parent_tsn,
+        ]
 
-    # create rank
-    if rank not in ranks[kingdom_id]:
+        print(f'{line_number}\t{name}')
+        line_number = line_number + 1
 
-        if not ranks[kingdom_id]:
-            rank_id = 1
-        else:
-            rank_id = ranks[kingdom_id][max(ranks[kingdom_id], key=ranks[kingdom_id].get)] + 1
-
-        ranks[kingdom_id][rank] = rank_id
-
-    else:
-        rank_id = ranks[kingdom_id][rank]
-
-    if row[columns['common_name']] == name:
-        row[columns['common_name']] = ''
-
-    data = [
-        [
-            name,
-            row[columns['common_name']],
-            row[columns['author']],
-            row[columns['source']],
-        ],
-        rank_id,
-        [],  # children
-        parent_tsn,
-    ]
-
-    print(str(line_number) + "\t" + data[0][0])
-    line_number = line_number + 1
-
-    rows[kingdom_id][tsn] = data
-
-    if parent_tsn == 0:
-        del rows[kingdom_id][tsn][3]
-        root[kingdom_id] = tsn
-
-    elif parent_tsn in rows[kingdom_id]:
-        del rows[kingdom_id][tsn][3]
-        rows[kingdom_id][parent_tsn][2].append(tsn)
-
-rows_file.close()
 
 #
 print('Saving kingdoms')
-new_kingdoms = {}
-for kingdom_name, kingdom_id in kingdoms.items():
-    new_kingdoms[kingdom_id] = kingdom_name
-kingdoms = new_kingdoms
-
-with open(kingdoms_data, 'w') as file:
+with open(os.path.join(target_dir, 'kingdoms.json'), 'w') as file:
     file.write(json.dumps(kingdoms))
-
-#
-print('Saving ranks')
-new_ranks = {}  # fix ranks being in wrong order
-for kingdom_id, kingdom_ranks in ranks.items():
-
-    new_ranks[kingdom_id] = {}
-    parent_rank_id = 0
-
-    for rank_name in specify_ranks:
-
-        lower_case_rank_name = rank_name.lower()
-
-        if lower_case_rank_name not in kingdom_ranks:
-            continue
-
-        rank_id = kingdom_ranks[lower_case_rank_name]
-        new_ranks[kingdom_id][rank_id] = [rank_name, parent_rank_id]
-
-        parent_rank_id = rank_id
-
-ranks = new_ranks
-
-with open(ranks_data, 'w') as file:
-    file.write(json.dumps(ranks))
 
 #
 print('Fixing orders')
 orders_fixed = 0
 
-for kingdom_id, kingdom_data in rows.items():
+modified = True
+while modified:
+    modified = False
 
-    modified = True
+    for tsn, row in rows.items():
 
-    while modified:
-        modified = False
+        if len(row) < 4:
+            continue
 
-        for tsn, row in kingdom_data.items():
+        parent_tsn = row[3]
 
-            try:
-                parent_tsn = row[3]
-                del row[3]
-            except IndexError:
-                continue
+        if parent_tsn not in rows:
+            continue
 
-            try:
-                rows[kingdom_id][parent_tsn][2].append(tsn)
-            except KeyError:
-                continue
+        del row[3]
+        rows[parent_tsn][2].append(tsn)
+        modified = True
 
-            modified = False
+        print('.')
+        orders_fixed = orders_fixed + 1
 
-            print('Fixed order')
-            orders_fixed = orders_fixed + 1
-
-    rows[kingdom_id]['root'] = root[kingdom_id]
+print('Filtering out nodes without parents')
+kingdom_rank_id = specify_ranks.index('kingdom') + 1
+rows = {
+    tsn:row
+    for tsn,row in rows.items()
+    if len(row) < 4 or row[1]==kingdom_rank_id
+}
 
 print('Rows: %d\nOrder fixes: %d' % (line_number, orders_fixed))
 
@@ -293,9 +266,35 @@ print('Rows: %d\nOrder fixes: %d' % (line_number, orders_fixed))
 print('Saving data')
 Path(rows_destination).mkdir(parents=True, exist_ok=True)
 
-for kingdom_id, rows_data in rows.items():
-    with open(rows_destination + str(kingdom_id) + '.json', 'w') as file:
-        file.write(json.dumps(rows_data))
+def group(tsn):
+    grouped_records.add(tsn)
+    kingdom_ranks.add(rows[tsn][1])
+    for child in rows[tsn][2]:
+        group(child)
 
-print('Begin time: %f' % begin_time)
-print('End time: %f' % time.time())
+ranks_data = {}
+for kingdom_id in kingdoms.keys():
+    grouped_records = set()
+    kingdom_ranks = set()
+    group(kingdom_id)
+
+    with open(os.path.join(rows_destination, f'{kingdom_id}.json'), 'w') as file:
+        file.write(json.dumps({
+            tsn:rows
+            for tsn, rows in rows.items()
+            if tsn in kingdom_ranks
+        }))
+
+    parent_rank_id = 0
+    ranks_data[kingdom_id] = {}
+    for rank_id, rank_name in enumerate(specify_ranks, start=1):
+        if rank_id not in kingdom_ranks:
+            continue
+        ranks_data[kingdom_id][rank_id] = [rank_name, parent_rank_id]
+        parent_rank_id = rank_id
+
+with open(os.path.join(target_dir, 'ranks.json'), 'w') as file:
+    file.write(json.dumps(ranks_data))
+
+print('Updated!')
+print('Time taken: %fs' % (time.time() - begin_time))
